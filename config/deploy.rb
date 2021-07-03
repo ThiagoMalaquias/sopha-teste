b095ac9029105b3ee54cae802819e4b4f2b5949a
Thiago Malaquias <tammalaquias@gmail.com>
2021-07-03 20:52:49 -0300

    fix config capistrano branch

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,7 +8,7 @@ set :application, 'sopha-teste'
 set :repo_url, 'git@github.com:ThiagoMalaquias/sopha-teste.git'
 #FIXME add location on the server here
 set :deploy_to, '/home/deploy/sopha-teste'
-set :branch, ENV['BRANCH'] if ENV['BRANCH']
+set :branch, "main"
 
 set :linked_files, %w{config/database.yml config/master.key}
 set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
